3909cc2837967cfdffc34a5a07bd6029c55eaaeb
LennyAnders <hey@lenny.fyi>
2023-02-20 23:44:35 +0100

    convert all content script to func instead of file

--- menuActions/sauceNao.js
+++ menuActions/sauceNao.js
@@ -12,7 +12,38 @@ export const createSauceNaoMenuItem = (parentId) => {
     if (isFile(srcUrl)) {
       chrome.scripting.executeScript({
         target: { tabId: tab.id },
-        files: ['contentScripts/sauceNao.js'],
+        func: async () => {
+          const image = document.querySelector('img');
+          const canvas = document.createElement('canvas');
+          const context = canvas.getContext('2d');
+
+          canvas.width = image.naturalWidth;
+          canvas.height = image.naturalHeight;
+          context.drawImage(image, 0, 0);
+
+          const type = 'image/jpeg';
+          const imageResponse = await fetch(canvas.toDataURL(type, 1));
+          const imageBuffer = await imageResponse.arrayBuffer();
+
+          const dataTransfer = new DataTransfer();
+          dataTransfer.items.add(new File([imageBuffer], image.src.split('/').pop(), { type }));
+
+          const form = document.createElement('form');
+          form.action = 'https://saucenao.com/search.php';
+          form.method = 'POST';
+          form.enctype = 'multipart/form-data';
+          form.target = '_blank';
+          form.style.display = 'none';
+
+          const input = document.createElement('input');
+          input.type = 'file';
+          input.name = 'file';
+          input.files = dataTransfer.files;
+
+          form.append(input);
+          document.body.append(form);
+          form.submit();
+        },
       });
       return;
     }
--- menuActions/tinEye.js
+++ menuActions/tinEye.js
@@ -59,7 +59,21 @@ export const createTinEyeMenuItem = (parentId) => {
       chrome.runtime.onMessage.addListener(searchFileOnTinEye);
       chrome.scripting.executeScript({
         target: { tabId: tab.id },
-        files: ['contentScripts/tinEye.js'],
+        func: () => {
+          const image = document.querySelector('img');
+          const canvas = document.createElement('canvas');
+          const context = canvas.getContext('2d');
+
+          canvas.width = image.naturalWidth;
+          canvas.height = image.naturalHeight;
+          context.drawImage(image, 0, 0);
+
+          chrome.runtime.sendMessage({
+            command: 'search-on-tineye',
+            image: canvas.toDataURL('image/jpeg', 1),
+            filename: image.src.split(' / ').pop(),
+          });
+        },
       });
       return;
     }
